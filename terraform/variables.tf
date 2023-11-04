74daacc52ca08ff0f41bfcd9091722560829be6e
Kurien Joseph <kjkurien@gmail.com>
2023-11-04 01:11:50 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "AZQUARKUS_RG"
+  default = "azquarkus-rg"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "AZQUARKUS"
+  default     = "azquarkus"
 }
 
 variable "location" {
